1e157b3db566208bb322815290010eabe5c944d8
Shamimzz <shamimislam5091@gmail.com>
2021-10-12 11:03:16 +0600

    added

--- src/components/HeaderFooter/Footer.js
+++ src/components/HeaderFooter/Footer.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 // Showing Footer Section...
-
 const Footer = () => {
     return (
           <div className="Footer">
@@ -22,18 +21,18 @@ const Footer = () => {
              <Col>
               <div className="mt-2">
                 <p className="text-light p-2 mr-5 text-decoration-none"><h5>COURSES</h5> </p>
-                <p><Link className="text-light p-2 mr-5 text-decoration-none" >SEO Business</Link></p>
-                <p><Link className="text-light p-2 mr-5 text-decoration-none" >Digital Marketing</Link></p>
-                <p><Link className="text-light p-2 mr-5 text-decoration-none" >Graphic Design</Link></p>
-                <p><Link className="text-light p-2 mr-5 text-decoration-none" >Social Marketing</Link></p>
+                <p><Link  to="*" className="text-light p-2 mr-5 text-decoration-none" >SEO Business</Link></p>
+                <p><Link to="*" className="text-light p-2 mr-5 text-decoration-none" >Digital Marketing</Link></p>
+                <p><Link to="*" className="text-light p-2 mr-5 text-decoration-none" >Graphic Design</Link></p>
+                <p><Link to="*" className="text-light p-2 mr-5 text-decoration-none" >Social Marketing</Link></p>
              </div>
              </Col>
              <Col>
               <div className="mt-2">
                 <p className="text-light p-2 mr-5 text-decoration-none"><h4>Legal</h4></p>
-                <p><Link className="text-light p-2 mr-5 text-decoration-none" >Terms & Condition</Link></p>
-                <p><Link className="text-light p-2 mr-5 text-decoration-none" >Privacy policy</Link></p>
-                <p><Link className="text-light p-2 mr-5 text-decoration-none" >Return policy</Link></p>              
+                <p><Link to="*" className="text-light p-2 mr-5 text-decoration-none" >Terms & Condition</Link></p>
+                <p><Link to="*" className="text-light p-2 mr-5 text-decoration-none" >Privacy policy</Link></p>
+                <p><Link to="*" className="text-light p-2 mr-5 text-decoration-none" >Return policy</Link></p>              
              </div>
              </Col>         
           </Row> 
